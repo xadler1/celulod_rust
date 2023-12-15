2ec086f26c306def6798ed12dd81b0eef5ecbf68
Jan Adler <469190@mail.muni.cz>
2023-12-15 23:57:08 +0000

    Gather sensor statistics

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use gphoto2::{Context, Result};
-use std::env;
+use std::fs::OpenOptions;
+use std::fs;
 use std::io::Write;
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
 use std::sync::mpsc;
-use std::sync::mpsc::Sender;
-use std::sync::mpsc::Receiver;
+use std::sync::mpsc::TryRecvError;
 use rppal::gpio::Gpio;
 use rppal::gpio::Trigger;
 use rppal::gpio::Level;
@@ -15,7 +15,7 @@ use rppal::gpio::Level;
 
 const STATES_LOW: [Level; 8] = [Level::Low; 8];
 const STATES_HIGH: [Level; 8] = [Level::High; 8];
-const STOP_COUNT: u64 = 64;
+const STOP_COUNT: u64 = 16;
 const POLLING_FREQUENCY_MS: u64 = 3;
 const POLLING_FREQUENCY_US: u64 = 125;
 const GPIO_INPUT: u8 = 3;
@@ -26,6 +26,7 @@ fn main() -> Result<()>
 {
 	// init section
 	println!("Hello, world!");
+	stop_motor();
 
 
 	// first capture takes a lot longer
@@ -33,36 +34,41 @@ fn main() -> Result<()>
 
 
 	//pin_input.set_interrupt(Trigger::FallingEdge);
-	let (tx_from_feedback, rx_from_feedback) = mpsc::channel();
-	let (tx_from_capture, rx_from_capture) = mpsc::channel();
+	let (tx_from_feedback, rx_from_feedback) = mpsc::channel::<Result<Option<u64>>>();
+	let (tx_from_capture, rx_from_capture) = mpsc::channel::<Result<Option<u64>>>();
 	let (tx_from_main_stop, rx_from_main_stop) = mpsc::channel();
 	let (tx_from_main_start, rx_from_main_start) = mpsc::channel();
 	let (tx_from_main_status, rx_from_main_status) = mpsc::channel();
 	let mut count: u64 = 1;
+	let mut debug_stats: Vec<Level> = vec![];
 
 	let capture_thread = thread::spawn(move || -> Result<()> {
 		let mut camera = Context::new()?.autodetect_camera().wait().expect("Failed to autodetect camera");
 		let mut capture_name = format!("capture_{count:0>9}.arw").to_string();
 		let mut file;
 
-		while let Some(_) = rx_from_feedback.recv().unwrap() {
-			file = camera.capture_image().wait()?;
+		loop {
+			match rx_from_feedback.recv().unwrap() {
+				Ok(_) => {file = camera.capture_image().wait()?;
 
-			camera
-				.fs()
-				.download_to(&file.folder(), &file.name(), Path::new(&capture_name))
-				.wait()?;
-			println!("Downloaded image {}", capture_name);
+				camera
+					.fs()
+					.download_to(&file.folder(), &file.name(), Path::new(&capture_name))
+					.wait()?;
+				//println!("Downloaded image {}", capture_name);
 
-			count += 1;
-			capture_name = format!("capture_{count:0>9}.arw").to_string();
+				count += 1;
+				capture_name = format!("capture_{count:0>9}.arw").to_string();
 
-			// Renew camera context
-			drop(camera);
-			camera = Context::new()?.autodetect_camera().wait().expect("Failed to autodetect camera");
+				// Renew camera context
+				drop(camera);
+				camera = Context::new()?.autodetect_camera().wait().expect("Failed to autodetect camera");
 
-			// Signal that capture is complete
-			tx_from_capture.send(Some(1));
+				// Signal that capture is complete
+				tx_from_capture.send(Ok(Some(1)));
+				},
+				Err(_) => break,
+			}
 		}
 
 		Ok(())
@@ -77,31 +83,43 @@ fn main() -> Result<()>
     let mut stop_count = STOP_COUNT;
 
     let control_thread = thread::spawn(move || {
-        println!("Start control_thread");
-        while let Some(stop_count) = rx_from_main_start.recv().unwrap() {
+	    loop {
+        while let Some(message) = rx_from_main_start.recv().unwrap() {
+		stop_count = message;
             break;
         }
 
-        println!("control_thread start signal received");
+        println!("Capturing {} images", stop_count);
+	let mut now = Instant::now();
+	let mut waited: u128 = 0;
+	let mut file = OpenOptions::new().write(true).create(true).append(true).open("stats.txt").unwrap();
 	    loop {
 	    	pin_output.set_high();
 	    	thread::sleep(Duration::from_micros(POLLING_FREQUENCY_US));
 
 	    	pin_input_states[15] = pin_input.read();
+		debug_stats.push(pin_input_states[15]);
 
-	    	if !capturing && is_falling(pin_input_states) {
+	    	if !capturing && is_rising(pin_input_states) {
 	    		// Capture image
-	    		tx_from_feedback.send(Some(1));
+	    		tx_from_feedback.send(Ok(Some(1)));
 	    		capturing = true;
+			now = Instant::now();
 	    	}
 
-	    	if capturing && is_rising(pin_input_states) {
-	    		pin_output.set_low();
+	    	if capturing && is_falling(pin_input_states) {
+			waited = now.elapsed().as_millis();
+			//thread::sleep(Duration::from_millis((175 - waited) as u64));
 
+	    		pin_output.set_low();
+			println!("{}", waited);
 	    		// wait for capture feedback
-	    		while let Some(_) = rx_from_capture.recv().unwrap() {
-	    			break;
-	    		}
+			loop {
+				match rx_from_capture.recv().unwrap() {
+					Ok(_) => break,
+					Err(_) => break,
+				}
+			}
 
 	    		signal_counter += 1;
 	    		if stop_count != 0 && signal_counter >= stop_count {
@@ -110,23 +128,38 @@ fn main() -> Result<()>
 
 	    		capturing = false;
 
-                if let Some(_) = rx_from_main_status.try_recv().unwrap() {
-                    println!("Captured {} of {} images.", signal_counter, stop_count);
-                }
+			match rx_from_main_status.try_recv() {
+				Ok(_) => {
+					println!("Captured {} of {} images.", signal_counter, stop_count);
+				},
+				Err(TryRecvError::Disconnected) => {},
+				Err(TryRecvError::Empty) => {},
+			}
 
-                if let Some(_) = rx_from_main_stop.try_recv().unwrap() {
-                    println!("Captured {} images.", signal_counter);
+			match rx_from_main_stop.try_recv() {
+				Ok(_) => {
+					println!("Captured {} images.", signal_counter);
+					break;
+				},
+				Err(TryRecvError::Disconnected) => {},
+				Err(TryRecvError::Empty) => {},
+			}
 
-	    		    while let Some(stop_count) = rx_from_main_start.recv().unwrap() {
-	    		    	break;
-	    		    }
-                }
+			for i in 0..debug_stats.len() {
+				let mut symbol: u8 = 40;
+				if (debug_stats[i] == Level::High) {
+					symbol = 41;
+				}
+				file.write_all(&[symbol]);
+			}
+			debug_stats = vec![];
 	    	}
 
 	    	for i in 0..15 {
 	    		pin_input_states[i] = pin_input_states[i + 1];
 	    	}
 
+	    }
 	    }
     });
 
@@ -152,7 +185,7 @@ fn main() -> Result<()>
 	stop_motor();
 
 
-	capture_thread.join().unwrap()?;
+	//capture_thread.join().unwrap()?;
 
 	println!("The wait is over.");
 	Ok(())
